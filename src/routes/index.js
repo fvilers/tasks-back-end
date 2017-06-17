7b8b7634bd5f042fb6dd3600ec630ce6eab78b69
Fabian Vilers <fabian.vilers@dev-one.com>
2017-06-17 08:41:09 +0200

    Make regex case insensitive

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.param('userId', (req, res, next, val) => {
-  const regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
+  const regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
   const err = regex.test(val) ? null : 'route';
   
   next(err);
