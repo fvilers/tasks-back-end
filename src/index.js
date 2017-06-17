51f3f270adfb4048c07cb4cab43944483b0173bd
Fabian Vilers <fabian.vilers@dev-one.com>
2017-06-17 08:01:31 +0200

    Add helmet

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
 
 const express = require('express');
 const compression = require('compression');
+const helmet = require('compression');
 const app = express();
 
 // Middlewares
 app.use(compression());
+app.use(helmet());
 app.use('*', (req, res) => res.sendStatus(404));
 app.use((err, req, res, next) => res.status(err.status || 500).send(err));
 
