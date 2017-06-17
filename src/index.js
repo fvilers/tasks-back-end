2ec80922d11c07af028c074be19b40c15c76f331
Fabian Vilers <fabian.vilers@dev-one.com>
2017-06-17 08:01:04 +0200

    Add compression

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const express = require('express');
+const compression = require('compression');
 const app = express();
 
 // Middlewares
+app.use(compression());
 app.use('*', (req, res) => res.sendStatus(404));
 app.use((err, req, res, next) => res.status(err.status || 500).send(err));
 
